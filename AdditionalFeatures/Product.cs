b3b80a036c02de0c203348c9f522aab5086ca26e
Supreetmalhi <malhisupreet40@gmail.com>
2021-09-18 00:07:32 +0530

    src code added

---
+++ AdditionalFeatures/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdditionalFeatures
+{
+    class Product
+    {
+        int ID { get; set; }
+        int Quantity { get; set; }
+        string ProductName { get; set; }
+        
+        string Description { get; set; }
+      
+
+        public Product(int _id, string _ProductName)
+            : this(_id, 0, _ProductName,"")
+        {
+
+        }
+
+        public Product(int _id, int _Quantity, string _ProductName)
+            :this(_id, _Quantity, _ProductName,"")
+        {
+            
+        }
+
+        public Product(int _id, int _Quantity, string _ProductName, string _Description)
+        {
+            ID = _id;
+            Quantity=_Quantity;
+            ProductName = _ProductName;
+            Description = _Description;
+         
+        }
+
+    }
+}
---
+++ AdditionalFeatures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AdditionalFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            const int a = 13;
+            var  name = "supreet";
+            Console.WriteLine(a + name);
+
+            Product _product= new Product(1,"Shoes");
+            Product _product2 = new Product(2,0,"Belt");
+        }
+    }
+}
